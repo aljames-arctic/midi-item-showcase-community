542a41fd64110bb2390dad8a07eef0765402392c
bakanabaka <3474820+aljames-arctic@users.noreply.github.com>
2024-10-12 11:55:21 -0500

    Update template.js
    Update template.js
    
    Bug Fix: Update circle to correct for missing center attribute
    New Feature: Add new template targeting function

--- scripts/automations/macros/bakanaUtils/template.js
+++ scripts/automations/macros/bakanaUtils/template.js
@@ -8,8 +8,8 @@ async function circle(positionEntity, range, color) {
       {
         t: 'circle',
         user: game.user.id,
-        x: positionEntity.center.x || positionEntity.x,
-        y: positionEntity.center.y || positionEntity.y,
+        x: positionEntity.center?.x ?? positionEntity.x,
+        y: positionEntity.center?.y ?? positionEntity.y,
         direction: 0,
         distance: distance,
         borderColor: '#000000',
@@ -20,4 +20,33 @@ async function circle(positionEntity, range, color) {
   return rangeTemplates[0];
 }
 
-export const templateApi = { circle };
+function targets(template) {
+  function getTargets(shape){
+    const allTokens = canvas.tokens.placeables.filter(obj => obj);
+    const targetArray = allTokens.filter(obj => {
+      const c = obj.center;
+      return shape.contains(c.x, c.y);
+    });
+    return targetArray;
+  }
+
+  let shape;
+  if(template.t === CONST.MEASURED_TEMPLATE_TYPES.CIRCLE) {
+    const ratio = canvas.scene.dimensions.distancePixels;
+    shape = new PIXI.Circle(template.x, template.y, template.distance * ratio);
+  }
+  else if(template.t === CONST.MEASURED_TEMPLATE_TYPES.RECTANGLE) {
+    shape = new PIXI.Rectangle(template.x, template.y, template.width, template.height);
+  }
+  else if(template.t === CONST.MEASURED_TEMPLATE_TYPES.CONE || template.t === CONST.MEASURED_TEMPLATE_TYPES.RAY) {
+    shape = new PIXI.Polygon(template.shape.points.map((p,i) => {
+      if(i%2 === 0) return p+template.x;
+      else return p+template.y;
+    }));
+  }
+
+  if(!shape) return;
+  return getTargets(shape);
+}
+
+export const templateApi = { circle, targets };
